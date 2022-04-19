dbf845d5785ae7a4d5bd4f5ec5bb763b46f875f0
류지헌 <rjhwind94@gmail.com>
2022-04-19 16:22:00 +0900

    JPA 이용시 listener 활성화

--- src/main/java/org/zerock/guestbook/GuestbookApplication.java
+++ src/main/java/org/zerock/guestbook/GuestbookApplication.java
@@ -2,8 +2,10 @@ package org.zerock.guestbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing // JPA 이용시 Listener 활성화를 위한 어노테이션
 public class GuestbookApplication {
 
 	public static void main(String[] args) {
