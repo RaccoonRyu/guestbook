460a82962137bb8139aec5c89280262182c0ac48
류지헌 <rjhwind94@gmail.com>
2022-04-22 10:55:04 +0900

    목록 데이터 페이징 처리 테스트용 모듈 작성

--- src/test/java/org/zerock/guestbook/service/GuestBookServiceTest.java
+++ src/test/java/org/zerock/guestbook/service/GuestBookServiceTest.java
@@ -37,14 +37,23 @@ public class GuestBookServiceTest {
     @Test
     public void testList() { // Entity가 DTO로 변환되었는지 확인
         PageRequestDTO pageRequestDTO = PageRequestDTO.builder()
-                .page(1)
+                .page(1) // 페이지 번호는 1
                 .size(10)
                 .build();
 
         PageResultDTO<GuestBookDTO, GuestBook> gbResultDTO = guestBookService.getList(pageRequestDTO);
 
+        System.out.println("PREV : "+gbResultDTO.isPrev()); // 1p이므로 이전으로 가는 링크 필요 없음
+        System.out.println("NEXT : "+gbResultDTO.isNext()); // 다음 페이지로 가는 링크는 필요
+        System.out.println("TOTAL : "+gbResultDTO.getTotalPage()); // 전체 페이지 개수
+
+        System.out.println("---------------------------------------------");
         for(GuestBookDTO guestbookDTO : gbResultDTO.getDtoList()) {
             System.out.println(guestbookDTO);
         }
+
+        System.out.println("=============================================");
+        gbResultDTO.getPageList().forEach(i->System.out.println(i));
     }
+
 }
